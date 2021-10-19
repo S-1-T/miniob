543848dd4f19173cd0ab4605cf74311cbbbbc600
JmPotato <ghzpotato@gmail.com>
2021-10-19 13:04:59 +0800

    使用 2 空格缩进
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- src/observer/storage/common/condition_filter.cpp
+++ src/observer/storage/common/condition_filter.cpp
@@ -24,262 +24,262 @@ See the Mulan PSL v2 for more details. */
 using namespace common;
 
 const bool ConditionFilter::field_type_compare_compatible_table[5][5] = {
-    {0, 0, 0, 0, 0}, /* UNDEFINE 与其他任何一种类型不可比较 */
-    {0, 1, 0, 0, 1}, /* CHARS 目前仅可与自身和 DATES 比较 */
-    {0, 0, 1, 0, 0}, /* INTS 目前仅可与自身比较（TODO：支持 FLOATS） */
-    {0, 0, 0, 1, 0}, /* FLOATS 目前仅可与自身比较（TODO：支持 INTS） */
-    {0, 1, 0, 0, 1}  /* DATES 目前仅可与自身和 CHARS 比较 */
+  {0, 0, 0, 0, 0}, /* UNDEFINE 与其他任何一种类型不可比较 */
+  {0, 1, 0, 0, 1}, /* CHARS 目前仅可与自身和 DATES 比较 */
+  {0, 0, 1, 0, 0}, /* INTS 目前仅可与自身比较（TODO：支持 FLOATS） */
+  {0, 0, 0, 1, 0}, /* FLOATS 目前仅可与自身比较（TODO：支持 INTS） */
+  {0, 1, 0, 0, 1} /* DATES 目前仅可与自身和 CHARS 比较 */
 };
 
 ConditionFilter::~ConditionFilter() {}
 
 DefaultConditionFilter::DefaultConditionFilter() {
-    left_.is_attr = false;
-    left_.attr_length = 0;
-    left_.attr_offset = 0;
-    left_.value = nullptr;
+  left_.is_attr = false;
+  left_.attr_length = 0;
+  left_.attr_offset = 0;
+  left_.value = nullptr;
 
-    right_.is_attr = false;
-    right_.attr_length = 0;
-    right_.attr_offset = 0;
-    right_.value = nullptr;
+  right_.is_attr = false;
+  right_.attr_length = 0;
+  right_.attr_offset = 0;
+  right_.value = nullptr;
 }
 DefaultConditionFilter::~DefaultConditionFilter() {}
 
 RC DefaultConditionFilter::init(const ConDesc &left, const ConDesc &right,
-                                AttrType attr_type, CompOp comp_op) {
-    if (attr_type < CHARS || attr_type > DATES) {
-        LOG_ERROR("Invalid condition with unsupported attribute type: %d",
-                  attr_type);
-        return RC::INVALID_ARGUMENT;
-    }
+                AttrType attr_type, CompOp comp_op) {
+  if (attr_type < CHARS || attr_type > DATES) {
+    LOG_ERROR("Invalid condition with unsupported attribute type: %d",
+          attr_type);
+    return RC::INVALID_ARGUMENT;
+  }
 
-    if (comp_op < EQUAL_TO || comp_op >= NO_OP) {
-        LOG_ERROR("Invalid condition with unsupported compare operation: %d",
-                  comp_op);
-        return RC::INVALID_ARGUMENT;
-    }
+  if (comp_op < EQUAL_TO || comp_op >= NO_OP) {
+    LOG_ERROR("Invalid condition with unsupported compare operation: %d",
+          comp_op);
+    return RC::INVALID_ARGUMENT;
+  }
 
-    left_ = left;
-    right_ = right;
-    attr_type_ = attr_type;
-    comp_op_ = comp_op;
-    return RC::SUCCESS;
+  left_ = left;
+  right_ = right;
+  attr_type_ = attr_type;
+  comp_op_ = comp_op;
+  return RC::SUCCESS;
 }
 
 RC DefaultConditionFilter::init(Table &table, const Condition &condition) {
-    const TableMeta &table_meta = table.table_meta();
-    ConDesc left;
-    ConDesc right;
+  const TableMeta &table_meta = table.table_meta();
+  ConDesc left;
+  ConDesc right;
 
-    AttrType type_left = UNDEFINED;
-    AttrType type_right = UNDEFINED;
+  AttrType type_left = UNDEFINED;
+  AttrType type_right = UNDEFINED;
 
-    if (1 == condition.left_is_attr) {
-        left.is_attr = true;
-        const FieldMeta *field_left =
-            table_meta.field(condition.left_attr.attribute_name);
-        if (nullptr == field_left) {
-            LOG_WARN("No such field in condition. %s.%s", table.name(),
-                     condition.left_attr.attribute_name);
-            return RC::SCHEMA_FIELD_MISSING;
-        }
-        left.attr_length = field_left->len();
-        left.attr_offset = field_left->offset();
-
-        left.value = nullptr;
-
-        type_left = field_left->type();
-    } else {
-        left.is_attr = false;
-        left.value = condition.left_value.data;  // 校验type 或者转换类型
-        type_left = condition.left_value.type;
-
-        left.attr_length = 0;
-        left.attr_offset = 0;
+  if (1 == condition.left_is_attr) {
+    left.is_attr = true;
+    const FieldMeta *field_left =
+      table_meta.field(condition.left_attr.attribute_name);
+    if (nullptr == field_left) {
+      LOG_WARN("No such field in condition. %s.%s", table.name(),
+           condition.left_attr.attribute_name);
+      return RC::SCHEMA_FIELD_MISSING;
     }
+    left.attr_length = field_left->len();
+    left.attr_offset = field_left->offset();
 
-    if (1 == condition.right_is_attr) {
-        right.is_attr = true;
-        const FieldMeta *field_right =
-            table_meta.field(condition.right_attr.attribute_name);
-        if (nullptr == field_right) {
-            LOG_WARN("No such field in condition. %s.%s", table.name(),
-                     condition.right_attr.attribute_name);
-            return RC::SCHEMA_FIELD_MISSING;
-        }
-        right.attr_length = field_right->len();
-        right.attr_offset = field_right->offset();
-        type_right = field_right->type();
+    left.value = nullptr;
 
-        right.value = nullptr;
-    } else {
-        right.is_attr = false;
-        right.value = condition.right_value.data;
-        type_right = condition.right_value.type;
+    type_left = field_left->type();
+  } else {
+    left.is_attr = false;
+    left.value = condition.left_value.data;  // 校验type 或者转换类型
+    type_left = condition.left_value.type;
 
-        right.attr_length = 0;
-        right.attr_offset = 0;
+    left.attr_length = 0;
+    left.attr_offset = 0;
+  }
+
+  if (1 == condition.right_is_attr) {
+    right.is_attr = true;
+    const FieldMeta *field_right =
+      table_meta.field(condition.right_attr.attribute_name);
+    if (nullptr == field_right) {
+      LOG_WARN("No such field in condition. %s.%s", table.name(),
+           condition.right_attr.attribute_name);
+      return RC::SCHEMA_FIELD_MISSING;
     }
+    right.attr_length = field_right->len();
+    right.attr_offset = field_right->offset();
+    type_right = field_right->type();
 
-    // 校验和转换
-    if (!field_type_compare_compatible_table[type_left][type_right]) {
+    right.value = nullptr;
+  } else {
+    right.is_attr = false;
+    right.value = condition.right_value.data;
+    type_right = condition.right_value.type;
+
+    right.attr_length = 0;
+    right.attr_offset = 0;
+  }
+
+  // 校验和转换
+  if (!field_type_compare_compatible_table[type_left][type_right]) {
+    return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+  if (type_left == DATES && type_right == CHARS) {
+    type_right = DATES;
+    if (right.value != nullptr) {
+      try {
+        Date date = Date(static_cast<char *>(right.value));
+        time_t date_time_t = date.get_inner_date_time_t();
+        memcpy(right.value, &date_time_t, sizeof(date_time_t));
+      } catch (const char *e) {
+        LOG_ERROR(
+          "Invalid right value data to convert into a Date type. "
+          "e=%s",
+          e);
         return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+      }
     }
-    if (type_left == DATES && type_right == CHARS) {
-        type_right = DATES;
-        if (right.value != nullptr) {
-            try {
-                Date date = Date(static_cast<char *>(right.value));
-                time_t date_time_t = date.get_inner_date_time_t();
-                memcpy(right.value, &date_time_t, sizeof(date_time_t));
-            } catch (const char *e) {
-                LOG_ERROR(
-                    "Invalid right value data to convert into a Date type. "
-                    "e=%s",
-                    e);
-                return RC::SCHEMA_FIELD_TYPE_MISMATCH;
-            }
-        }
-    } else if (type_left == CHARS && type_right == DATES) {
-        type_left = DATES;
-        if (left.value != nullptr) {
-            try {
-                Date date = Date(static_cast<char *>(left.value));
-                time_t date_time_t = date.get_inner_date_time_t();
-                memcpy(left.value, &date_time_t, sizeof(date_time_t));
-            } catch (const char *e) {
-                LOG_ERROR(
-                    "Invalid right value data to convert into a Date type. "
-                    "e=%s",
-                    e);
-                return RC::SCHEMA_FIELD_TYPE_MISMATCH;
-            }
-        }
+  } else if (type_left == CHARS && type_right == DATES) {
+    type_left = DATES;
+    if (left.value != nullptr) {
+      try {
+        Date date = Date(static_cast<char *>(left.value));
+        time_t date_time_t = date.get_inner_date_time_t();
+        memcpy(left.value, &date_time_t, sizeof(date_time_t));
+      } catch (const char *e) {
+        LOG_ERROR(
+          "Invalid right value data to convert into a Date type. "
+          "e=%s",
+          e);
+        return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+      }
     }
-    return init(left, right, type_left, condition.comp);
+  }
+  return init(left, right, type_left, condition.comp);
 }
 
 bool DefaultConditionFilter::filter(const Record &rec) const {
-    char *left_value = nullptr;
-    char *right_value = nullptr;
+  char *left_value = nullptr;
+  char *right_value = nullptr;
 
-    if (left_.is_attr) {  // value
-        left_value = (char *)(rec.data + left_.attr_offset);
-    } else {
-        left_value = (char *)left_.value;
+  if (left_.is_attr) {  // value
+    left_value = (char *)(rec.data + left_.attr_offset);
+  } else {
+    left_value = (char *)left_.value;
+  }
+
+  if (right_.is_attr) {
+    right_value = (char *)(rec.data + right_.attr_offset);
+  } else {
+    right_value = (char *)right_.value;
+  }
+
+  int cmp_result = 0;
+  switch (attr_type_) {
+    case CHARS: {  // 字符串都是定长的，直接比较
+      // 按照C字符串风格来定
+      cmp_result = strcmp(left_value, right_value);
+    } break;
+    case INTS: {
+      // 没有考虑大小端问题
+      // 对int和float，要考虑字节对齐问题,有些平台下直接转换可能会跪
+      int left = *(int *)left_value;
+      int right = *(int *)right_value;
+      cmp_result = left - right;
+    } break;
+    case FLOATS: {
+      float left = *(float *)left_value;
+      float right = *(float *)right_value;
+      cmp_result = (int)(left - right);
+    } break;
+    case DATES: {
+      time_t left = *(time_t *)left_value;
+      time_t right = *(time_t *)right_value;
+      cmp_result = (int)(left - right);
+    } break;
+    default: {
     }
+  }
 
-    if (right_.is_attr) {
-        right_value = (char *)(rec.data + right_.attr_offset);
-    } else {
-        right_value = (char *)right_.value;
-    }
+  switch (comp_op_) {
+    case EQUAL_TO:
+      return 0 == cmp_result;
+    case LESS_EQUAL:
+      return cmp_result <= 0;
+    case NOT_EQUAL:
+      return cmp_result != 0;
+    case LESS_THAN:
+      return cmp_result < 0;
+    case GREAT_EQUAL:
+      return cmp_result >= 0;
+    case GREAT_THAN:
+      return cmp_result > 0;
 
-    int cmp_result = 0;
-    switch (attr_type_) {
-        case CHARS: {  // 字符串都是定长的，直接比较
-            // 按照C字符串风格来定
-            cmp_result = strcmp(left_value, right_value);
-        } break;
-        case INTS: {
-            // 没有考虑大小端问题
-            // 对int和float，要考虑字节对齐问题,有些平台下直接转换可能会跪
-            int left = *(int *)left_value;
-            int right = *(int *)right_value;
-            cmp_result = left - right;
-        } break;
-        case FLOATS: {
-            float left = *(float *)left_value;
-            float right = *(float *)right_value;
-            cmp_result = (int)(left - right);
-        } break;
-        case DATES: {
-            time_t left = *(time_t *)left_value;
-            time_t right = *(time_t *)right_value;
-            cmp_result = (int)(left - right);
-        } break;
-        default: {
-        }
-    }
+    default:
+      break;
+  }
 
-    switch (comp_op_) {
-        case EQUAL_TO:
-            return 0 == cmp_result;
-        case LESS_EQUAL:
-            return cmp_result <= 0;
-        case NOT_EQUAL:
-            return cmp_result != 0;
-        case LESS_THAN:
-            return cmp_result < 0;
-        case GREAT_EQUAL:
-            return cmp_result >= 0;
-        case GREAT_THAN:
-            return cmp_result > 0;
-
-        default:
-            break;
-    }
-
-    LOG_PANIC("Never should print this.");
-    return cmp_result;  // should not go here
+  LOG_PANIC("Never should print this.");
+  return cmp_result;  // should not go here
 }
 
 CompositeConditionFilter::~CompositeConditionFilter() {
-    if (memory_owner_) {
-        delete[] filters_;
-        filters_ = nullptr;
-    }
+  if (memory_owner_) {
+    delete[] filters_;
+    filters_ = nullptr;
+  }
 }
 
 RC CompositeConditionFilter::init(const ConditionFilter *filters[],
-                                  int filter_num, bool own_memory) {
-    filters_ = filters;
-    filter_num_ = filter_num;
-    memory_owner_ = own_memory;
-    return RC::SUCCESS;
+                  int filter_num, bool own_memory) {
+  filters_ = filters;
+  filter_num_ = filter_num;
+  memory_owner_ = own_memory;
+  return RC::SUCCESS;
 }
 RC CompositeConditionFilter::init(const ConditionFilter *filters[],
-                                  int filter_num) {
-    return init(filters, filter_num, false);
+                  int filter_num) {
+  return init(filters, filter_num, false);
 }
 
 RC CompositeConditionFilter::init(Table &table, const Condition *conditions,
-                                  int condition_num) {
-    if (condition_num == 0) {
-        return RC::SUCCESS;
-    }
-    if (conditions == nullptr) {
-        return RC::INVALID_ARGUMENT;
-    }
+                  int condition_num) {
+  if (condition_num == 0) {
+    return RC::SUCCESS;
+  }
+  if (conditions == nullptr) {
+    return RC::INVALID_ARGUMENT;
+  }
 
-    RC rc = RC::SUCCESS;
-    ConditionFilter **condition_filters = new ConditionFilter *[condition_num];
-    for (int i = 0; i < condition_num; i++) {
-        DefaultConditionFilter *default_condition_filter =
-            new DefaultConditionFilter();
-        rc = default_condition_filter->init(table, conditions[i]);
-        if (rc != RC::SUCCESS) {
-            delete default_condition_filter;
-            for (int j = i - 1; j >= 0; j--) {
-                delete condition_filters[j];
-                condition_filters[j] = nullptr;
-            }
-            delete[] condition_filters;
-            condition_filters = nullptr;
-            return rc;
-        }
-        condition_filters[i] = default_condition_filter;
+  RC rc = RC::SUCCESS;
+  ConditionFilter **condition_filters = new ConditionFilter *[condition_num];
+  for (int i = 0; i < condition_num; i++) {
+    DefaultConditionFilter *default_condition_filter =
+      new DefaultConditionFilter();
+    rc = default_condition_filter->init(table, conditions[i]);
+    if (rc != RC::SUCCESS) {
+      delete default_condition_filter;
+      for (int j = i - 1; j >= 0; j--) {
+        delete condition_filters[j];
+        condition_filters[j] = nullptr;
+      }
+      delete[] condition_filters;
+      condition_filters = nullptr;
+      return rc;
     }
-    return init((const ConditionFilter **)condition_filters, condition_num,
-                true);
+    condition_filters[i] = default_condition_filter;
+  }
+  return init((const ConditionFilter **)condition_filters, condition_num,
+        true);
 }
 
 bool CompositeConditionFilter::filter(const Record &rec) const {
-    for (int i = 0; i < filter_num_; i++) {
-        if (!filters_[i]->filter(rec)) {
-            return false;
-        }
+  for (int i = 0; i < filter_num_; i++) {
+    if (!filters_[i]->filter(rec)) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
